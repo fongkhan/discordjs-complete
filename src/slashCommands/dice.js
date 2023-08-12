ebf936c0ba7f2a51248b45d6717919dbf4ff307a
fongkhan <fong.vu@hotmail.fr>
2023-08-13 00:59:48 +0200

    update

--- src/slashCommands/dice.js
+++ src/slashCommands/dice.js
@@ -5,27 +5,27 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('dice')
 		.setDescription('Roll a dice with a number of faces')
-        .addStringOption(option =>
+		.addStringOption(option =>
 			option.setName('dice')
 				.setDescription('dice to roll (ex: 1d6 or 2d10 + 5d20))')
 				.setRequired(true))
-        .addStringOption(option =>
-			option.setName('exlpode')
+		.addStringOption(option =>
+			option.setName('explode')
 				.setDescription('explode dice (ex: 6)')
 				.setRequired(false))
-        .addStringOption(option =>
+		.addStringOption(option =>
 			option.setName('keep')
 				.setDescription('keep dice (ex: 2)')
 				.setRequired(false))
-        .addStringOption(option =>
+		.addStringOption(option =>
 			option.setName('adding')
 				.setDescription('adding fixed value (ex: 5)')
 				.setRequired(false)),
 	async execute(interaction) {
-        const dice = interaction.options.getString('dice');
-        const explode = interaction.options.getString('explode');
-        const keep = interaction.options.getString('keep');
-        const adding = interaction.options.getString('adding');
+		const dice = interaction.options.getString('dice');
+		const explode = interaction.options.getString('explode');
+		const keep = interaction.options.getString('keep');
+		const adding = interaction.options.getString('adding');
 		functions.rollDice(interaction, dice, explode, keep, adding);
 	},
 };
