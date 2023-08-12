5565f8595867dfc281bab9ff7245395cdd2aabcb
fongkhan <fong.vu@hotmail.fr>
2023-08-13 01:12:27 +0200

    deleting dice and formatting roll

--- src/slashCommands/roll.js
+++ src/slashCommands/roll.js
@@ -7,8 +7,8 @@ module.exports = {
 		.setDescription('Entrez la formule du dé personnalisé (ex: d6, d20, 2d8, etc.)')
 		.addStringOption(option => option.setName('dice').setDescription('Dice to roll').setRequired(true)),
 	async execute(interaction) {
-        const roll = new Roll();
-        const result = roll.roll(interaction.options.getString('dice'));
+		const roll = new Roll();
+		const result = roll.roll(interaction.options.getString('dice'));
 		return interaction.reply({ content: `Résultat du lancer: ${result.result}\nDétail: ${result.rolled}` });
 	},
 };
